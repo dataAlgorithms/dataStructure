61f43fe9790fd0a07c3f75f87e4a1e974a2e694b
dataAlgorithms <195527444@qq.com>
2016-08-15 10:07:58 +0800

    Chain hash table

--- hashTable.py
+++ hashTable.py
@@ -1,3 +1,4 @@
+1. Common hash table
 class HashTable:
     def __init__(self, size):
         self.size = size
@@ -127,3 +128,202 @@ if __name__ == "__main__":
     print(H.slots)    
     print(H.data)    
     print H[99]
+
+2. Chain hash table
+class LinkedList:
+
+    def push(self, new, prev = None):
+        if prev == None:
+            new.next = self.head
+            self.head = new
+        else:
+            new.next = prev.next
+            prev.next = new
+
+    def pop(self, index = 0):
+        cur = index
+        prev_node = None
+        cur_node = self.head
+        while cur > 0:
+            prev_node = cur_node
+            cur_node = cur_node.next
+            cur -= 1
+            
+        if prev_node == None:
+            popped = self.head
+            self.head = self.head.next
+            return popped
+        else:
+            prev_node = cur_node.next
+            return cur_node
+
+    def insert(self, node, index = 0):
+        if node == None:
+            raise Exception("node is None Type")
+            return
+        cur = index
+        prev_node = None
+        cur_node = self.head
+        while cur > 0:
+            prev_node = cur_node
+            cur_node = cur_node.next
+            cur -= 1
+
+        if prev_node == None:
+            self.head = node
+        else:
+            prev_node.next = node
+
+        node.next = cur_node
+
+    def __str__(self):
+        if self.head == None:
+            return ""
+        else:
+            return str(self.head)
+
+    def __init__(self, head = None):
+        self.head = head
+
+
+class Link:
+    """ Link
+
+        Used in the ChainedHashtable, a Link is a key, value pair for use in a linked list.
+    """
+
+    def __str__(self):
+        if self.next == None:
+            return str(self.value) + " "
+        else:
+            return str(self.value) + " " + str(self.next)
+
+    def __init__(self, key = 0, value = 0, next = None):
+        self.key = key
+        self.value = value
+        self.next = next
+
+def DivisionHash(key, size):
+    return key % size
+
+class HashTable:
+
+    size = 23
+
+    # Init
+    def __init__(self):
+        self.links = [None] * HashTable.size
+
+    def __str__(self):
+        lines = []
+        for i in range(len(self.links)):
+            if self.links[i] is None:
+                lines.append("" + str(i) + "\t")
+            else:
+                lines.append("" + str(i) + "\t" + str(self.links[i]))
+
+        return "\n".join(lines)
+
+    def put(self, key, value):
+        llist = self.links[self.hash(key)]
+        if llist is None:
+            node = Link(key=key, value=value)
+            llist = LinkedList(head=node)
+            self.links[self.hash(key)] = llist
+            return
+        cur_node = llist.head
+        while cur_node is not None:
+            if cur_node.key == key:
+                cur_node.value = value
+                return
+            else:
+                cur_node = cur_node.next
+        llist.push(Link(key=key, value=value))
+
+    def get(self, key):
+        llist = self.links[self.hash(key)]
+        if llist is None:
+            return None
+
+        cur_node = llist.head
+        while cur_node is not None:
+            if cur_node.key == key:
+                return cur_node.value
+            else:
+                cur_node = cur_node.next
+        return None
+
+    def search1(self, key):
+        llist = self.links[self.hash(key)]
+        if llist is None:
+            return str(self.hash(key)) 
+        search_result = ""
+        cur_node = llist.head
+        search_result += str(self.hash(key)) + " "
+        while cur_node is not None:
+            search_result += str(cur_node.value) + " "
+            if cur_node.key == key:
+                return search_result
+            else:
+                cur_node = cur_node.next
+        return search_result
+    
+    def search(self, key):
+        llist = self.links[self.hash(key)]
+        if llist == None:
+            return str(self.hash(key))
+        search_result = ""
+        cur_node = llist.head
+        search_result += str(self.hash(key)) + " " 
+        while cur_node != None:
+            search_result += str(cur_node.value) + " "
+            if cur_node.key == key:
+                return search_result
+            else:
+                cur_node = cur_node.next
+        return search_result
+
+
+    def insert(self,value):
+        self.put(value, value)
+
+    def hash(self, key):
+        return DivisionHash(key, HashTable.size)
+
+'''
+0	
+1	1 
+2	
+3	
+4	
+5	
+6	
+7	
+8	100 
+9	
+10	
+11	
+12	12 81 35 
+13	
+14	
+15	
+16	
+17	
+18	
+19	
+20	
+21	21 
+22	
+12
+12 12 81 35 
+
+'''
+if __name__ == "__main__":
+    theValues = [100, 1, 21, 35, 81, 12]
+    htable = HashTable()
+    for value in theValues:
+        htable.insert(value)
+
+    print htable
+    print htable.get(12)
+    print htable.search(35)
