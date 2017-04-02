af180fe844b43378e2f405d1e2895662f901df4e
dataAlgorithms <195527444@qq.com>
2017-04-02 08:26:05 +0800

    Create findLoopBegin.py

---
+++ linkedList/findLoopBegin.py
@@ -0,0 +1,217 @@
+'''
+Check whether the given linked list is None-terminated or not
+If there is a cycle, find the start node of the loop.
+'''
+
+def findLoopBegin(head):
+
+    slowPtr = head
+    fastPtr = head
+    loopExists = 0
+
+    while slowPtr and fastPtr and fastPtr.next:
+        slowPtr = slowPtr.next
+        fastPtr = fastPtr.next.next
+        if slowPtr == fastPtr:
+            loopExists = 1
+            break
+
+    if loopExists == 1:
+        slowPtr = head
+        while slowPtr != fastPtr:
+            fastPtr = fastPtr.next
+            slowPtr = slowPtr.next
+
+        return slowPtr
+
+    return 
+
+class _SingleLinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class _SingleLinkedListIter:
+    def __init__(self, head):
+        self.curNode = head 
+    def __iter__(self):
+        return self
+    def next(self):
+        if self.curNode is not None:
+            curNode = self.curNode 
+            self.curNode = self.curNode.next 
+            return curNode.data 
+        else:
+            raise StopIteration
+    
+class SingleLinkedList:
+    def __init__(self):
+        self._head = None
+        self._count = 0
+
+    # Get the length of single linked list
+    def __len__(self):  
+        return self._count
+    
+    # Get the item
+    def __getitem__(self, pos):
+        assert pos >= -1 and pos < len(self), \
+                "invalid position!"
+        preNode = self._head
+        curNode = self._head 
+        index = 0
+        while curNode is not None and index < pos:
+            preNode = curNode
+            curNode = curNode.next 
+            index += 1
+            
+        return preNode.data 
+    
+    # Get the length of single linked list
+    def listLength(self):  
+        return self._count
+
+    # Check whether the sinlge linked list is empty
+    def listIsEmpty(self):
+        return self.listLength() == 0
+
+    # Insert node into single linked list  
+    def listNodeInsertBak(self, data, position):
+        
+        newNode = _SingleLinkedListNode(data)
+        if position == 0: # insert a node at the front
+            newNode.next = self._head 
+            self._head = newNode 
+        elif position == -1: # insert a node at the end
+            preNode = self._head
+            curNode = self._head 
+            while curNode is not None:
+                preNode = curNode 
+                curNode = curNode.next 
+                
+            preNode.next = newNode 
+        else: # insert a node at middle
+            listLen = self.listLength()
+            assert position >= 1 and position <= listLen, \
+                "position:%s is out of list length:%s" % (position, listLen)
+                
+            preNode = self._head 
+            curNode = self._head
+            index = 0 
+            while curNode is not None and index < position:
+                preNode = curNode 
+                curNode = curNode.next 
+                index += 1
+                
+            newNode.next = curNode 
+            preNode.next = newNode 
+            
+        self._count += 1
+
+    def listNodeInsert(self, data, position):
+        assert position >= -1 and position <= len(self), \
+             "Position is out of list length."
+        newNode = _SingleLinkedListNode(data)
+        if len(self) == 1:
+            assert position == 0 or position == -1, \
+                "position is out of range!"
+            if position == 0:
+                newNode.next = self._head 
+                self._head = newNode
+            elif position == -1:
+                self._head.next = newNode  
+                newNode.prev = self._head
+        else:        
+            if position == 0:
+                if self.listIsEmpty():
+                    self._head = newNode 
+                else:
+                    newNode.next = self._head
+                    self._head = newNode
+            elif position == -1:
+                preNode = self._head
+                curNode = self._head
+                while curNode is not None:
+                    preNode = curNode
+                    curNode = curNode.next
+
+                preNode.next = newNode
+            else:                 
+                preNode = self._head
+                curNode = self._head
+                index = 0
+                while curNode.next is not None and index < position:
+                    preNode = curNode
+                    curNode = curNode.next
+                    index += 1
+
+                newNode.next = curNode
+                preNode.next = newNode
+
+        self._count += 1    
+                        
+    # Delete node from single linked list
+    def listNodeDelete(self, position):
+        
+        assert not self.listIsEmpty(), "Single linked list is empty!"
+        if self.listLength() == 1:
+            assert position == 0 or position == -1, "position should be 0 or -1 when list length is 1!"
+            self._head = None
+        else:
+            if position == 0:
+                self._head = self._head.next 
+            elif position == -1:
+                preNode = self._head 
+                curNode = self._head 
+                while curNode.next is not None:
+                    preNode = curNode 
+                    curNode = curNode.next 
+                
+                preNode.next = curNode.next 
+            else:
+                preNode = self._head 
+                curNode = self._head 
+                index = 0
+            
+                assert position >=1 and position < self.listLength(), \
+                   "positon:%s is out of list length:%s" % (position, self.listLength())
+                while curNode.next is not None and index < position:
+                    preNode = curNode 
+                    curNode = curNode.next 
+                    index += 1
+                
+                preNode.next = curNode.next    
+            
+        self._count -= 1
+
+    def listTraversal(self):
+        curNode = self._head 
+        while curNode is not None:
+            print "%s " % curNode.data,
+            curNode = curNode.next 
+        print '\r'
+
+    def listClear(self):
+        listLen = self.listLength()
+        for _ in range(listLen):
+            self.listNodeDelete(0)
+
+    def __iter__(self):
+        return _SingleLinkedListIter(self._head)
+    
+'''
+None
+e
+'''
+if __name__ == "__main__":
+    sll = SingleLinkedList()
+    sll.listNodeInsert('a', 0)
+    sll.listNodeInsert('b', 0)
+    sll.listNodeInsert('c', 0) 
+    sll.listNodeInsert('d', 0) 
+    sll.listNodeInsert('e', 0) 
+    
+    print findLoopBegin(sll._head)
+    
+    sll._head.next.next.next.next = sll._head
+    print findLoopBegin(sll._head).data
