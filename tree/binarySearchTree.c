e07ff243dc9d38437bbbba102601a4bf330d1d2a
dataAlgorithms <195527444@qq.com>
2017-07-31 13:54:48 +0800

    Update binarySearchTree.c

--- tree/binarySearchTree.c
+++ tree/binarySearchTree.c
@@ -58,3 +58,127 @@ void PreOrderNonRecursive(struct BinaryTreeNode *root) {
 
     deleteStack(S);
 }
+
+/*
+Algorithms:
+Traverse the left subtree in Inorder
+Visit the root
+Traverse the right subtree in Inorder
+
+Complexity:
+Time: O(n)
+Space: O(n)
+*/
+void InOrder(struct BinaryTreeNode *root) {
+    if (root) {
+        InOrder(root->left);
+        printf("%d", root->data);
+        InOrder(root->right);
+    }
+}
+
+/*
+Algorithms:
+
+Complexity:
+Time: O(n)
+Space: O(n)
+*/
+void InOrderNonRecursive(struct BinaryTreeNode *root) {
+    struct Stack *s = createStack();
+
+    while (1) {
+        while (root) {
+            push(s, root);
+            
+            //Got left subtree and keep on adding to stack
+            root = root->left;
+        }
+
+        if (isEmptyStack(s))
+            break;
+
+        root = pop(s);
+        printf("%d", root->data);
+
+        //Indicates completion of left subtree and current node
+        //now to go to right subtree
+        root = root->right;
+    }
+
+    deleteStack(s);
+}
+
+/*
+Algo:
+Traverse the left subtree in Postorder
+Traverse the right subtree in Postorder
+Visit the root
+
+Complexity:
+Time: O(n)
+Space: O(n)
+*/
+void PostOrder(struct BinaryTreeNode *root) {
+    if (root) {
+        PostOrder(root->left);
+        PostOrder(root->right);
+        printf("%d", root->data);
+    }
+}
+
+void PostOrderNonRecursive(struct BinaryTreeNode *root) {
+    struct Stack *s = createStack();
+    struct BinaryTreeNode *previous = NULL;
+
+    do {
+        while (root != NULL) {
+            push(s, root);
+            root = root->left;
+        }
+
+        while (root == NULL && !isEmptyStack(s)) {
+            root = top(s);
+            if(root->right == NULL || root->right == previous) {
+                printf("%d ", root->data);
+                pop(s);
+                previous = root;
+                root = NULL;
+            }
+            else
+                root = root->right;
+        }
+    } while(!isEmptyStack(s));
+}
+
+/*
+Algo:
+Visit the root
+While traversing level (, keep all the elements at level (+1 in queue
+Go to next level and visit all the nodes at that level
+Repeat this until all levels are completed
+
+Complexity:
+Time: O(n)
+Space: O(n)
+*/
+void LevelOrder(struct BinaryTreeNode *root) {
+    struct BinaryTreeNode *temp;
+    struct Queue *q = createQueue();
+
+    if (!root)
+        return;
+
+    enQueue(q, root);
+    while (!isEmptyQueue(q)) {
+        temp = deQueue(q);
+        //Process current node
+        printf("%d", temp->data);
+        if (temp->left)
+            enQueue(q, temp->left);
+        if (temp->right)
+            enQueue(q, temp->right);
+    }
+
+    deleteQueue(q);
+}
